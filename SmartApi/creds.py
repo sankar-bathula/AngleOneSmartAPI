2a0c20dc55d50f358f294681de3bc5acabe21443
Shankar Bathula <41106397+sankar-bathula@users.noreply.github.com>
2026-02-10 20:27:40 +0530

    Update creds.py
    updated credentials

--- SmartApi/creds.py
+++ SmartApi/creds.py
@@ -1,6 +1,7 @@
-api_key = '6ybPZHNp'
-historicalapi_key = "XBdQETpl"
-historicalapi_secrate = "324f0379-b674-46db-96c3-2080d92ee6ec"
-client_code = "B352053"
-client_pin = "9046"
-totp_code = "H6D2W7QO7EEG3JOKCVVZVZIQYQ"
+api_key = ''
+historicalapi_key = ""
+historicalapi_secrate = ""
+client_code = ""
+client_pin = ""
+totp_code = ""
+
